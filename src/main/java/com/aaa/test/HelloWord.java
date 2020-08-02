ac2d43a39be5561bdce4be015c8bd6793973a96b
qinweiwei <qinweiwei@ruigushop.com>
2020-08-02 22:14:54 +0800

    test分支第一次提交

--- src/main/java/com/aaa/test/HelloWord.java
+++ src/main/java/com/aaa/test/HelloWord.java
@@ -13,5 +13,7 @@ public class HelloWord {
         System.out.println("hello world");
         System.out.println("hello world");
         System.out.println("hello world");
+        System.out.println("hello world");
+
     }
 }
