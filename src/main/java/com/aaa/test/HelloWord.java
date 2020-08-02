77bd2c4843418956181e6b061573cd1c12a9d2da
qinweiwei <qinweiwei@ruigushop.com>
2020-08-02 22:31:34 +0800

    master四次提交到本地 仓库

--- src/main/java/com/aaa/test/HelloWord.java
+++ src/main/java/com/aaa/test/HelloWord.java
@@ -14,6 +14,7 @@ public class HelloWord {
         System.out.println("hello world");
         System.out.println("hello world");
         System.out.println("hello world");
+        System.out.println("hello world");
 
     }
 }
