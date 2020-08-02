fe9835049a5b9c8da1a33d79923d75fe6c1111d0
qinweiwei <qinweiwei@ruigushop.com>
2020-08-02 21:40:49 +0800

    master第一次提交

---
+++ src/main/java/com/aaa/test/HelloWord.java
@@ -0,0 +1,14 @@
+package com.aaa.test;
+
+/**
+ * @Author Qin
+ * @Date 2020/8/2 21:37
+ * @Description
+ **/
+public class HelloWord {
+
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
