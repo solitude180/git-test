b04053df5be11dc25ddcc14ea99b80552eb72563
qinweiwei <qinweiwei@ruigushop.com>
2020-08-30 22:57:26 +0800

    fix: 3

--- src/main/java/com/aaa/test/HelloWord.java
+++ src/main/java/com/aaa/test/HelloWord.java
@@ -44,6 +44,11 @@ public class HelloWord {
 
         System.out.println("0830-qinweiwei提交第四次");
 
+        System.out.println("test第二次");
+
+
+
+
         System.out.println("0830-qinweiwei提交第五次");
 
         System.out.println("0830-master提交第三次");
